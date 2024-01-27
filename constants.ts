d0438484e6b2c3686434876576a40a89358d503d
Jason Gardner <im@jasongardner.co>
2024-01-27 12:31:15 -0600

    upd: Reduce MAX_HEIGHT in deploy

--- constants.ts
+++ constants.ts
@@ -1,3 +1,5 @@
 export const BLOCK_VERSION = 18090528;
 export const DEFAULT_BLOCK = "minecraft:air";
-export const MAX_HEIGHT = 256;
+export const MAX_HEIGHT = Deno.env.get("DENO_DEPLOYMENT_ID") !== undefined
+  ? 64
+  : 256;
