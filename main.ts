7ead01bc72a510bc967b1890bde3342704fec2ce
Jason Gardner <im@jasongardner.co>
2024-01-30 09:53:23 -0600

    Revert "upd: Resolve db from meta URL"
    This reverts commit ad0ba1025d879feabd1f83de619b35afb5502fd1.

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import { createStructure } from "./mod.ts";
 import decode from "./_decode.ts";
 import createPalette from "./_palette.ts";
 import { basename, extname, join } from "./deps.ts";
+import defaultDb from "./db/minecraft.json" with { type: "json" };
 
 export default async function main(
   imgSrc: string,
@@ -26,12 +27,14 @@ export default async function main(
 
 if (import.meta.main) {
   const [src, axis, db] = Deno.args;
-  const colorDb: Record<string, string> = (await import(
-    db ?? (new URL("./db/minecraft.json", import.meta.url)).href,
-    {
-      with: { type: "json" },
-    }
-  )).default;
+  const colorDb: Record<string, string> = db
+    ? (await import(
+      db,
+      {
+        with: { type: "json" },
+      }
+    )).default
+    : defaultDb;
 
   if (Deno.args.length > 0) {
     const fileName = basename(src, extname(src));
