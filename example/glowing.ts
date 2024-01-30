5f44eefb0f05595ca32774a137a7c71396a11702
Jason Gardner <im@jasongardner.co>
2024-01-30 09:26:58 -0600

    Squashed commit of the following:
    commit 4aa4008675fc6bf046767ec93c33b2d4deefd57c
    Author: Jason Gardner <im@jasongardner.co>
    Date:   Tue Jan 30 07:41:44 2024 -0600
    
        upd(examples): Fixed glowing palette
    
    commit a6987176063910951f2831fba41e57cd63c13db9
    Author: Jason Gardner <im@jasongardner.co>
    Date:   Tue Jan 30 07:41:13 2024 -0600
    
        upd(examples): Add offset to setBlock
    
        - Change axis output
        - Add offsets
    
    commit afe16ec5c532414d71c9e2f150d528c0e34a6aa3
    Author: Jason Gardner <im@jasongardner.co>
    Date:   Tue Jan 30 06:15:23 2024 -0600
    
        upd(examples): Added RGB examples
    
        - Add examples with RGB add-on
    
    commit 27e953c51b9670a27bdd232015ffdf7b8c26d24d
    Author: Jason Gardner <im@jasongardner.co>
    Date:   Mon Jan 29 18:12:03 2024 -0600
    
        feat: Block states
    
    commit 9f8beb8047636086972ca1ffb95251605a1b7bb4
    Author: Jason Gardner <im@jasongardner.co>
    Date:   Mon Jan 29 16:00:04 2024 -0600
    
        upd: Examples with custom database
    
    commit 29736e5f2dea75b7ca190aca82fc33a53b39b13a
    Author: Jason Gardner <im@jasongardner.co>
    Date:   Mon Jan 29 14:05:57 2024 -0600
    
        feat: Add custom color db option

--- example/glowing.ts
+++ example/glowing.ts
@@ -1,6 +1,13 @@
 import type { Axis } from "../types.ts";
 import { nanoid } from "../deps.ts";
 import main from "../main.ts";
+import db from "../db/rainbow.json" with { type: "json" };
+
+const palette = Object.fromEntries(
+  Object.keys(db).filter((id) => id.includes("lit") || id.includes("lamp")).map(
+    (id) => [id, db[id as keyof typeof db]],
+  ),
+);
 
 const structureId = nanoid(6);
 
@@ -8,8 +15,7 @@ await Deno.writeFile(
   `./glowing_${structureId}.mcstructure`,
   await main(
     Deno.args[0],
+    palette,
     (Deno.args[1] ?? "x") as Axis,
-    ({ id }) =>
-      id.startsWith("rainbow:") && (id.includes("lit") || id.includes("lamp")),
   ),
 );
