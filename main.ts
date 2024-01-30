845c6ef70f61edd49a402f476595612182781bd0
Jason Gardner <im@jasongardner.co>
2024-01-30 09:37:27 -0600

    fix: import.meta resolve

--- main.ts
+++ main.ts
@@ -28,9 +28,7 @@ if (import.meta.main) {
   const [src, axis, db] = Deno.args;
   const colorDb: Record<string, string> = (await import(
     db ??
-      toFileUrl(
-        join(import.meta.dirname ?? Deno.cwd(), "db", "minecraft.json"),
-      ),
+      import.meta.resolve("./db/minecraft.json"),
     {
       with: { type: "json" },
     }
