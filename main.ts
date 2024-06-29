88598526726a608a51b19c8db8ecea1c0ac0c4b3
Jason Gardner <im@jasongardner.co>
2024-06-29 11:42:54 -0500

    fix: argv count

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ import server from "./server.ts";
 import db from "./db/minecraft.json" with { type: "json" };
 import process from "node:process"
 
-if (process.argv.length > 0) {
+if (process.argv.length > 2) {
   await cli(
     process.argv[0],
     db,
