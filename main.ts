00cb2660d39b5df3efa661a04f9ecc14ec61929a
Jason Gardner <im@jasongardner.co>
2024-06-29 12:25:42 -0500

    fix: Deno Deploy site

--- main.ts
+++ main.ts
@@ -2,15 +2,14 @@ import type { Axis } from "./src/types.ts";
 import cli from "./src/mcstructure/cli.ts";
 import server from "./server.ts";
 import db from "./db/minecraft.json" with { type: "json" };
-import process from "node:process"
 
-if (process.argv.length > 2) {
+if (Deno.args.length > 2) {
   await cli(
-    process.argv[0],
+    Deno.args[0],
     db,
-    (process.argv[1] ?? "x") as Axis,
+    (Deno.args[1] ?? "x") as Axis,
   );
-  process.exit(0);
+  Deno.exit(0);
 }
 
 server(db);
