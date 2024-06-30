f6bc3a1372bd00fdc410624c67b836cb774faf1e
Jason Gardner <im@jasongardner.co>
2024-06-30 13:49:05 -0500

    upd: Imagescript from NPM

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * as imagescript from "https://deno.land/x/imagescript@1.3.0/ImageScript.js";
+export * as imagescript from "npm:imagescript";
 export * as nbt from "npm:nbtify@1.90.1"; //"https://cdn.jsdelivr.net/npm/nbtify/dist/index.min.js";
 export { nanoid } from "npm:nanoid@5.0.7";
 export { default as JSZip } from "npm:jszip@3.10.1";
