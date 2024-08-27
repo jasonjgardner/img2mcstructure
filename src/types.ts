34859dd95dc6f3c831dc69c960502d1420159f3c
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-08-27 13:01:20 -0700

    NBTify Structure Typings
    They are more specific primitives to what the underlying NBT uses, this will make things more safe when loading the file. I still have to bring this to the rest of the functions that implement around these types.
    
    Borrowed this from my Structure-Reader project!
    https://github.com/Offroaders123/Structure-Reader
    
    `block_position_data` still needs specific NBTify typings, I didn't have those in my reference code.

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import type { IntTag, StringTag } from "nbtify";
+
 export type Axis = "x" | "y" | "z";
 export type RGB = [number, number, number];
 export type RGBA = [number, number, number, number];
@@ -13,19 +15,19 @@ export interface IBlock {
 }
 
 export interface IMcStructure {
-  format_version: number;
-  size: [number, number, number];
-  structure_world_origin: [number, number, number];
+  format_version: IntTag;
+  size: [IntTag, IntTag, IntTag];
+  structure_world_origin: [IntTag, IntTag, IntTag];
   structure: {
-    block_indices: [number[], number[]];
+    block_indices: [IntTag[], IntTag[]];
     entities: Record<string, unknown>[];
     palette: {
       default: {
-        block_palette: Array<{
-          version: number;
-          name: string;
+        block_palette: {
+          version: IntTag;
+          name: StringTag;
           states: Record<string, unknown>;
-        }>;
+        }[];
         block_position_data: Record<string, Record<string, number | string>>;
       };
     };
