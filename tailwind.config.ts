fb12672f6b2d0ad65aca8edbb8b1f4c7de284ab0
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-08-27 11:55:10 -0700

    CLI + Frontend Removal
    Going back to the basics now (this is definitely opinionated, I like having a whole universal stack whenever possible)

--- tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('npm:tailwindcss').Config} */
-export default {
-  content: ["./public/**/*.html", "./app/**/*.{js,jsx,ts,tsx,css}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
